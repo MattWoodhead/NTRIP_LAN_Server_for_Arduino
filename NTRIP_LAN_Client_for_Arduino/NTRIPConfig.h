6543873314be4bded61a039744a26c6e8c210aea
Matt Woodhead <woody.w62@gmail.com>
2020-04-16 15:32:42 +0100

    Delete NTRIPConfig.h

--- NTRIP_LAN_Client_for_Arduino/NTRIPConfig.h
+++
@@ -1,5 +0,0 @@
-#define host "rtk2go.com"             //ntrip caster host address 
-#define httpPort 2101                 //port 2101 is default port of NTRIP caster
-#define mountPoint "ArduinoUnoRTCM"   //must change this string
-#define password "dyZAfi2q"          //enter password of ntrip caster for subscribing
-#define sourceString ""              //source table infomation string reference software.rtcm-ntrip.org/wiki : NTRIP sourcetable contents (https://software.rtcm-ntrip.org/wiki/Sourcetable)
