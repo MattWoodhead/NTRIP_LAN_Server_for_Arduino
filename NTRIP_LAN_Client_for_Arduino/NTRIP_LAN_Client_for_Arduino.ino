1295f6f809f794be9954aee55df3755c2003b1d7
Matt Woodhead <woody.w62@gmail.com>
2020-04-25 18:31:51 +0100

    initial changes to add 1008 message

--- NTRIP_LAN_Client_for_Arduino/NTRIP_LAN_Client_for_Arduino.ino
+++ NTRIP_LAN_Client_for_Arduino/NTRIP_LAN_Client_for_Arduino.ino
@@ -16,9 +16,19 @@ HardwareSerial & RtcmSerial = Serial1;                              // this is a
 
 // Randomly generated at https://www.hellion.org.uk/cgi-bin/randmac.pl 
 const byte mac_address[] = {0xA8,0x61,0x0a,0xAE,0x7E,0x6C};           // Note: Newer boards come with a fixed MAC address
-byte ip_address[] = {192,168,1,184};                                        // Assign an IP address if DHCP is not available
+byte ip_address[] = {192,168,1,184};                                  // Assign an IP address if DHCP is not available
+
 char char_buffer[512];                                                // buffer into which we will read the RtcmSerial port
 int char_count = 0;                                                   // counter to keep track of buffer length
+
+unsigned long previous_time = 0;                                      // timers to decide when to send 1008 message
+unsigned long latest_time = 0; 
+const int rtcm1008_rate = 10000;                                      // Rate at which to send 1008 message in milliseconds
+
+const byte rtcm1008[] = {                                             // This byte sends a 1008 message with ADVNULL as the antenna model
+0xd3,0x00,0x14,0x3f,0x00,0x00,0x0e,0x41,0x44,0x56,0x4e,0x55,0x4c,
+0x4c,0x41,0x4e,0x54,0x45,0x4e,0x4e,0x41,0x00,0x00,0x79,0x06,0x89
+};
 
 NTRIPServer ntrip_s;                                                  // Create an instance of the NTRIPServer class
 
@@ -31,16 +41,13 @@ void setup() {
   
   pinMode(4, OUTPUT);
   digitalWrite(4, HIGH);                                              // disable SD card
-
-//  pinMode(13, OUTPUT);
-//  digitalWrite(13, LOW);                                              // led
   
   delay(250);
 
   //Ethernet.begin(mac_address, ip_address);
-  Ethernet.begin(mac_address);                                    // Use this one if using DHCP
+  Ethernet.begin(mac_address);                                        // Use this one if using DHCP
   delay(100);
-  while (Ethernet.linkStatus() == LinkOFF) {                        // Wait for ethernet to connect
+  while (Ethernet.linkStatus() == LinkOFF) {                          // Wait for ethernet to connect
     delay(500);
     Serial.print(".");
   }
@@ -54,6 +61,7 @@ void setup() {
     delay(15000);
   }
   Serial.println(F("MountPoint Subscribed"));
+  previous_time = millis();
 }
 
 void loop() {
@@ -62,14 +70,22 @@ void loop() {
     while (RtcmSerial.available()) {
       char c = RtcmSerial.read();                                       // read in a byte as a character
       char_buffer[char_count++] = c;                                    // add character to buffer and increment counter
+      
       if ((c == '\n') || (char_count == sizeof(char_buffer)-1)) {       // if the buffer gets full or we see the newline character
         ntrip_s.write((uint8_t*)char_buffer, char_count);               // send buffer to the ntrip server
         char_count = 0;                                                 // reset counter
-        for (int i=0;i<sizeof(char_buffer);i++){                        // wipe the buffer for the next loop
+        for (unsigned int i=0;i<sizeof(char_buffer);i++){                        // wipe the buffer for the next loop
           char_buffer[i] = " ";
         //Serial.println(F("packet sent"));                             // for debugging only
         }
       }
+      
+      latest_time = millis();
+      if ((latest_time - previous_time) > rtcm1008_rate){               // if the specified length of time has passed since the last 1008 message was sent
+        ntrip_s.write(rtcm1008, sizeof(rtcm1008));                      // send the 1008 message to the ntrip server
+        previous_time = millis();                                       // reset the timer
+      }
+      
     }
   } else {
 //    digitalWrite(13, LOW);
