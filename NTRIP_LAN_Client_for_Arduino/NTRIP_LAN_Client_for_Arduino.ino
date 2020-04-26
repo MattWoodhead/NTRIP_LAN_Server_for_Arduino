2aa076054be73c34c8d34aa9ef4ee83ce8bf3a80
Matt Woodhead <woody.w62@gmail.com>
2020-04-26 16:45:21 +0100

    Update NTRIP_LAN_Client_for_Arduino.ino
    improved injection logic

--- NTRIP_LAN_Client_for_Arduino/NTRIP_LAN_Client_for_Arduino.ino
+++ NTRIP_LAN_Client_for_Arduino/NTRIP_LAN_Client_for_Arduino.ino
@@ -23,6 +23,8 @@ int char_count = 0;                                                   // counter
 unsigned long previous_time = 0;                                      // timers to decide when to send 1008 message
 unsigned long latest_time = 0; 
 const int rtcm1008_rate = 10000;                                      // Rate at which to send 1008 message in milliseconds
+bool rtcm1008_injected = false;                                       // A flag used to determine if the 1008 message has been sent successfully
+
 
 const byte rtcm1008[] = {                                             // This byte sends a 1008 message with ADVNULL as the antenna model
 0xd3,0x00,0x14,0x3f,0x00,0x00,0x0e,0x41,0x44,0x56,0x4e,0x55,0x4c,
@@ -35,7 +37,7 @@ void setup() {
   delay(500);
   Serial.begin(115200);
   Serial.println(F("Starting..."));
-  RtcmSerial.begin(115200);                                            //57600 causes issues - crashes arduino uno during ethernet connection
+  RtcmSerial.begin(115200);
   delay(250);
   
   pinMode(4, OUTPUT);
@@ -73,31 +75,42 @@ void loop() {
       if ((c == '\n') || (char_count == sizeof(char_buffer)-1)) {       // if the buffer gets full or we see the newline character
         ntrip_s.write((uint8_t*)char_buffer, char_count);               // send buffer to the ntrip server
         char_count = 0;                                                 // reset counter
-        for (unsigned int i=0;i<sizeof(char_buffer);i++){                        // wipe the buffer for the next loop
+        for (unsigned int i=0;i<sizeof(char_buffer);i++){               // wipe the buffer for the next loop
           char_buffer[i] = " ";
         //Serial.println(F("packet sent"));                             // for debugging only
         }
       }
       
       latest_time = millis();
-      if ((latest_time - previous_time) > rtcm1008_rate){               // if the specified length of time has passed since the last 1008 message was sent
-        ntrip_s.write(rtcm1008, sizeof(rtcm1008));                      // send the 1008 message to the ntrip server
-        previous_time = millis();                                       // reset the timer
+      if (not rtcm1008_injected) {                                      // if the rtcm1008 flag is false (i.e. time has elapsed or message not yet injected)
+        if ((c == 0xd3) && (char_count + sizeof(rtcm1008) < sizeof(char_buffer)-2)) {    // if the premable byte of a message is the latest to arrive at the serial port, and there is enough room in the buffer to inject the 1008 message
+          char_count--;                                                 // decrement the buffer counter so we overwrite the preamble character
+          for (unsigned int i=0;i<sizeof(rtcm1008);i++){                // add the 1008 message bytes to the buffer 
+            char_buffer[char_count+i] = rtcm1008[i];
+          }
+          char_count = char_count + sizeof(rtcm1008);                   // correct the counter for the operations performed in the loop and adding the preamble byte below
+          char_buffer[char_count-1] = c;                                // add the preamble byte back in
+          rtcm1008_injected = true;                                     // set the sent flag to true so the timer gets reset to delay the next 1008 transmission
+          previous_time = millis();                                     // reset the timer
+        }
+      } else {                                                          // if the rtcm1008 flag is true
+        if ((latest_time - previous_time) > rtcm1008_rate){             // check if the specified length of time has passed since the last 1008 message was sent
+          rtcm1008_injected = false;                                    // set the rtcm1008 flag to false so that the time is only checked once. This should present any rollover if the injection takes longer than the specified time period
+        }
       }
-      
+
     }
-  } else {
-//    digitalWrite(13, LOW);
-    ntrip_s.stop();
+  } else {                                                              // if ntrip connection fails
+    ntrip_s.stop();                                                     // close current socket connection
     Serial.println(F("reconnect"));
     delay(1000);                                                        // prevents reconnect spam
-    if (!ntrip_s.subStation(host, httpPort, mountPoint, password, sourceString)) {
-      delay(100);
+    if (!ntrip_s.subStation(host, httpPort, mountPoint, password, sourceString)) {      // try to reconnect
+      delay(1000);                                                      // prevents reconnect spam
     }
     else {
       Serial.println(F("MountPoint Subscribed"));
       delay(10);
     }
   }
-  delay(5);                                                            //server cycle
+  delay(1);                                                            // minimum cycle time (ms).
 }
