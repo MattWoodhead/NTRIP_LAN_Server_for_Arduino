59d52325489dd85699f3667ec99da6c7313d7572
Matt Woodhead <woody.w62@gmail.com>
2020-04-26 21:00:48 +0100

    Update NTRIP_LAN_Client_for_Arduino.ino

--- NTRIP_LAN_Client_for_Arduino/NTRIP_LAN_Client_for_Arduino.ino
+++ NTRIP_LAN_Client_for_Arduino/NTRIP_LAN_Client_for_Arduino.ino
@@ -17,7 +17,7 @@ HardwareSerial & RtcmSerial = Serial1;                              // this is a
 const byte mac_address[] = {0xA8,0x61,0x0a,0xAE,0x7E,0x6C};           // Note: Newer boards come with a fixed MAC address
 byte ip_address[] = {192,168,1,184};                                  // Assign an IP address if DHCP is not available
 
-char char_buffer[512];                                                // buffer into which we will read the RtcmSerial port
+char char_buffer[256];                                                // buffer into which we will read the RtcmSerial port
 int char_count = 0;                                                   // counter to keep track of buffer length
 
 unsigned long previous_time = 0;                                      // timers to decide when to send 1008 message
@@ -26,7 +26,7 @@ const int rtcm1008_rate = 10000;                                      // Rate at
 bool rtcm1008_injected = false;                                       // A flag used to determine if the 1008 message has been sent successfully
 
 
-const byte rtcm1008[] = {                                             // This byte sends a 1008 message with ADVNULL as the antenna model
+const uint8_t rtcm1008[] = {                                          // This byte sends a 1008 message with ADVNULL as the antenna model
 0xd3,0x00,0x14,0x3f,0x00,0x00,0x0e,0x41,0x44,0x56,0x4e,0x55,0x4c,
 0x4c,0x41,0x4e,0x54,0x45,0x4e,0x4e,0x41,0x00,0x00,0x79,0x06,0x89
 };
@@ -67,7 +67,6 @@ void setup() {
 
 void loop() {
   if (ntrip_s.connected()) {
-//    digitalWrite(13, HIGH);
     while (RtcmSerial.available()) {
       char c = RtcmSerial.read();                                       // read in a byte as a character
       char_buffer[char_count++] = c;                                    // add character to buffer and increment counter
@@ -83,10 +82,10 @@ void loop() {
       
       latest_time = millis();
       if (not rtcm1008_injected) {                                      // if the rtcm1008 flag is false (i.e. time has elapsed or message not yet injected)
-        if ((c == 0xd3) && (char_count + sizeof(rtcm1008) < sizeof(char_buffer)-2)) {    // if the premable byte of a message is the latest to arrive at the serial port, and there is enough room in the buffer to inject the 1008 message
+        if ((c == char(0xd3)) && (char_count + sizeof(rtcm1008) < sizeof(char_buffer)-2)) {    // if the premable byte of a message is the latest to arrive at the serial port, and there is enough room in the buffer to inject the 1008 message
           char_count--;                                                 // decrement the buffer counter so we overwrite the preamble character
           for (unsigned int i=0;i<sizeof(rtcm1008);i++){                // add the 1008 message bytes to the buffer 
-            char_buffer[char_count+i] = rtcm1008[i];
+            char_buffer[char_count+i] = char(rtcm1008[i]);
           }
           char_count = char_count + sizeof(rtcm1008);                   // correct the counter for the operations performed in the loop and adding the preamble byte below
           char_buffer[char_count-1] = c;                                // add the preamble byte back in
